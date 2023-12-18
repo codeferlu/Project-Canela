9a0656b64f9c3f7c238e1774e2f126e36748fe94
Fernando <codeferlu@gmail.com>
2023-12-18 14:49:12 -0300

    actualizacion de tabla y datos

--- assets/js/canela.js
+++ assets/js/canela.js
@@ -1,44 +1,64 @@
+// Variable para contar los clics en la imagen
 let clickCount = 0;
+
+// Variable para almacenar el usuario actual
 let currentUser = null;
+
+// Arreglo para almacenar los datos de usuario
 let userData = [];
 
+// Función que se ejecuta al hacer clic en la imagen
 function CanelaBark() {
-    clickCount++;
-    document.getElementById("count").innerText = clickCount;
+    clickCount++; // Incrementa el contador de clics
+    document.getElementById("count").innerText = clickCount; // Actualiza la visualización del contador
 
     if (currentUser) {
-        currentUser.clicks = clickCount;
-        updateRankingTable();
+        currentUser.clicks = clickCount; // Actualiza el número de clics del usuario actual
+        updateRankingTable(); // Actualiza la tabla de clasificación
     }
 }
 
+// Función para guardar los datos del usuario ingresados en el formulario
 function saveUserData() {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const country = document.getElementById("country").value;
+    event.preventDefault(); // Evita el comportamiento predeterminado del formulario
+    const name = document.getElementById("name").value; // Obtiene el nombre ingresado
+    const country = document.getElementById("country").value; // Obtiene el país ingresado
     
     if (name !== "" && country !== "") {
-        currentUser = {
-            name,
-            country,
-            clicks: 0 // Inicialmente los clics son 0
-        };
-        userData.push(currentUser);
-        clickCount = 0;
-        updateRankingTable();
+        let existingUserIndex = userData.findIndex(user => user.name === name && user.country === country);
+
+        if (existingUserIndex !== -1) {
+            // Si el usuario ya existe, se actualizan sus datos
+            currentUser = userData[existingUserIndex];
+        } else {
+            // Si es un usuario nuevo, se crea un objeto con los datos y se agrega al arreglo
+            currentUser = {
+                name,
+                country,
+                clicks: 0 // Inicialmente los clics son 0
+            };
+            userData.push(currentUser); // Agrega el usuario al arreglo de datos
+        }
+
+        clickCount = 0; // Reinicia el contador de clics
+        updateRankingTable(); // Actualiza la tabla de clasificación
     }
-    document.getElementById("loginForm").reset();
+    document.getElementById("loginForm").reset(); // Reinicia el formulario
 }
 
+// Función para actualizar la tabla de clasificación
 function updateRankingTable() {
     const rankingData = document.getElementById("rankingData");
+
+    userData.sort((a, b) => b.clicks - a.clicks); // Ordena los usuarios por número de clics (de mayor a menor)
+
+    // Limpia el contenido existente de la tabla
     rankingData.innerHTML = "";
 
-    userData.sort((a, b) => b.clicks - a.clicks);
-
+    // Recorre los datos de usuario y los agrega a la tabla
     userData.forEach(user => {
-        const row = document.createElement("tr");
-        row.innerHTML = `<td>${user.name}</td><td>${user.country}</td><td>${user.clicks}</td>`;
-        rankingData.appendChild(row);
+        const row = document.createElement("tr"); // Crea una nueva fila en la tabla
+        row.innerHTML = `<td>${user.name}</td><td>${user.country}</td><td>${user.clicks}</td>`; // Agrega celdas con los datos del usuario
+        rankingData.appendChild(row); // Agrega la fila a la tabla
     });
 }
